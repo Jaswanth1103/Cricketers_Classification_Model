29425b490312b6ec7ce976676d76ddc87ab910e0
Jaswanth1103 <jaswanthreddy1120@gmail.com>
2025-09-02 12:30:07 +0530

    Update cricketer classification app

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ app = Flask(__name__)
 def home():
     return "Cricketers Classification API is running!"
 
-@app.route('/classify_image', methods=['POST'])
+@app.route('/classify_image', methods=['GET', 'POST'])
 def classify_image_route():
+    if request.method == 'GET':
+        return ":Use PORT to classify images"
     image_data = request.form.get('image_data')
     if not image_data:
         return jsonify({'error': 'No image data received'}), 400
